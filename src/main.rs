1e2a4d75c2c48748ce14e7421bd4a4145a7ae07f
Takahiro Miyamoto <takahiro.miyamoto@glv.co.jp>
2019-10-16 16:17:02 +0900

    state

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,39 @@
-use actix_web::{server, App, HttpRequest, Responder};
+use actix_web::{server, App, Error, FromRequest, HttpRequest, Path, Responder, State};
+use serde_derive::*;
 
 fn hello(req: &HttpRequest) -> impl Responder {
     let to = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", to)
 }
 
+// Deserializeにしたがって抽出されるので型を用意しておく
+#[derive(Deserialize)]
+struct HelloPath {
+    name: String,
+}
+
+fn hello_name(to: Path<HelloPath>) -> impl Responder {
+    format!("Hello {}!", &to.name)
+}
+
+struct MyApp {
+    server_name: String,
+}
+
+fn hello_with_state(app: State<MyApp>) -> Result<String, Error> {
+    Ok(format!("Hello from {}", &app.server_name))
+}
+
 fn main() {
     server::new(|| {
-        App::new()
-            .resource("/", |r| r.f(hello))
-            .resource("/{name}", |r| r.f(hello))
+        // App::new()
+        // .resource("/", |r| r.f(hello))
+        // .resource("/{name}", |r| r.f(hello))
+        // .resource("/{name}", |r| r.with(hello_name))
+        App::with_state(MyApp {
+            server_name: "server with state".into(),
+        })
+        .resource("/info", |r| r.with(hello_with_state))
     })
     .bind("127.0.0.1:3000")
     .expect("Can not bind to port 3000")
